4d58211bdfc4cf3e99ab40a290bd339ba23073a6
ixtreeme <114311627+ixtreeme@users.noreply.github.com>
2026-04-17 15:52:36 +0200

    Phase 10: P10.0-A Add NO_LEGACY ECS event types

--- SRC/Server/GameServer/ecs/events.hpp
+++ SRC/Server/GameServer/ecs/events.hpp
@@ -51,4 +51,115 @@ struct EvAffectExpired {
     uint32_t affectType { 0 };
 };
 
+// War map lifecycle
+struct EvWarBegin {
+    uint32_t warId { 0 };
+};
+
+struct EvWarEnd {
+    uint32_t warId { 0 };
+};
+
+struct EvWarTimeout {
+    uint32_t warId { 0 };
+};
+
+// Dungeon lifecycle
+struct EvDungeonPrepare {
+    uint32_t dungeonId { 0 };
+};
+
+struct EvDungeonEnd {
+    uint32_t dungeonId { 0 };
+};
+
+struct EvDungeonDead {
+    uint32_t dungeonId { 0 };
+};
+
+// Session
+struct EvDescDisconnect {
+    uint32_t descHandle { 0 };
+};
+
+struct EvDescPing {
+    uint32_t descHandle { 0 };
+};
+
+// Mount
+struct EvMountSystemUpdate {
+    entt::entity mountEntity { entt::null };
+};
+
+// Guild
+struct EvGuildInvite {
+    uint32_t guildId { 0 };
+    uint32_t targetPid { 0 };
+};
+
+// Horse stamina
+struct EvHorseStaminaConsume {
+    entt::entity riderEntity { entt::null };
+};
+
+struct EvHorseStaminaRegen {
+    entt::entity riderEntity { entt::null };
+};
+
+// Combat
+struct EvStunBegin {
+    entt::entity entity { entt::null };
+    uint32_t durationMs { 0 };
+};
+
+struct EvCharDead {
+    entt::entity killer { entt::null };
+    entt::entity victim { entt::null };
+};
+
+// Affect / DOT
+struct EvFireApplied {
+    entt::entity entity { entt::null };
+    int32_t damage { 0 };
+};
+
+struct EvPoisonApplied {
+    entt::entity entity { entt::null };
+    int32_t damage { 0 };
+};
+
+struct EvBleedingApplied {
+    entt::entity entity { entt::null };
+    int32_t damage { 0 };
+};
+
+// Character/session lifecycle
+struct EvCharSaved {
+    entt::entity entity { entt::null };
+};
+
+struct EvCharDisconnect {
+    entt::entity entity { entt::null };
+};
+
+// Movement / recovery
+struct EvWarpBegin {
+    entt::entity entity { entt::null };
+    uint32_t mapIndex { 0 };
+    int32_t x { 0 };
+    int32_t y { 0 };
+};
+
+struct EvRecovery {
+    entt::entity entity { entt::null };
+    int32_t hpGain { 0 };
+    int32_t mpGain { 0 };
+};
+
+// Skill
+struct EvSkillUsed {
+    entt::entity entity { entt::null };
+    uint32_t skillId { 0 };
+};
+
 } // namespace ecs
