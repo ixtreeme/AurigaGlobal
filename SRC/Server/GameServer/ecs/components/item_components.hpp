4d3c02cb81c27efc399486348413f0b274038ba5
ixtreeme <114311627+ixtreeme@users.noreply.github.com>
2026-04-17 20:11:57 +0200

    Phase 11: P11.0-A Add item ECS component definitions

---
+++ SRC/Server/GameServer/ecs/components/item_components.hpp
@@ -0,0 +1,52 @@
+#pragma once
+
+#include <array>
+#include <cstdint>
+
+#include <common/length.h>
+#include <common/tables.h>
+
+namespace ecs {
+
+struct ItemIdentity {
+    uint32_t id { 0 };
+    uint32_t vnum { 0 };
+    uint32_t vid { 0 };
+    uint32_t maskVnum { 0 };
+};
+
+struct ItemLocation {
+    uint8_t window { 0 };
+    uint16_t cell { 0 };
+};
+
+struct ItemCount {
+    int count { 0 };
+};
+
+struct ItemOwner {
+    uint32_t ownerPID { 0 };
+    uint32_t lastOwnerPID { 0 };
+    uint32_t ownershipPID { 0 };
+};
+
+struct ItemEquipped {
+    bool equipped { false };
+};
+
+struct ItemFlags {
+    int32_t flags { 0 };
+    bool exchanging { false };
+    bool skipSave { false };
+    bool isLocked { false };
+};
+
+struct ItemSockets {
+    std::array<int32_t, ITEM_SOCKET_MAX_NUM> sockets {};
+};
+
+struct ItemAttributes {
+    std::array<TPlayerItemAttribute, ITEM_ATTRIBUTE_MAX_NUM> attrs {};
+};
+
+} // namespace ecs
