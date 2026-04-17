1df4eed56ce7fa7daffe86373ea454f38eb0b6e9
ixtreeme <114311627+ixtreeme@users.noreply.github.com>
2026-04-17 23:07:33 +0200

    Phase 13: migrate New_PetSystem.cpp

--- SRC/Server/GameServer/New_PetSystem.cpp
+++ SRC/Server/GameServer/New_PetSystem.cpp
@@ -3,7 +3,7 @@
 #include "stdafx.h"
 #include "utils.h"
 #include "vector.h"
-#include "char.h"
+#include "char_interface.hpp"
 #include "sectree_manager.h"
 #include "char_manager.h"
 #include "mob_manager.h"
@@ -1128,6 +1128,7 @@ bool CNewPetActor::_UpdatAloneActionAI(float fMinDist, float fMaxDist)
 // StateHorse함수 그냥 C&P -_-;
 bool CNewPetActor::_UpdateFollowAI()
 {
+	// INTERFACE_TODO: m_pkMobData direct access, needs public/null-safe mob-data query surface.
 	if (nullptr == m_pkChar->m_pkMobData)
 	{
 		//sys_err("[CPetActor::_UpdateFollowAI] m_pkChar->m_pkMobData is NULL");
