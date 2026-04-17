dbe2faa12968e8c65a23d31cd2b11797ffe83c34
ixtreeme <114311627+ixtreeme@users.noreply.github.com>
2026-04-17 23:04:32 +0200

    Phase 13: migrate input_p2p.cpp

--- SRC/Server/GameServer/input_p2p.cpp
+++ SRC/Server/GameServer/input_p2p.cpp
@@ -3,7 +3,7 @@
 #include "config.h"
 #include "desc_client.h"
 #include "desc_manager.h"
-#include "char.h"
+#include "char_interface.hpp"
 #include "char_manager.h"
 #include "p2p.h"
 #include "guild.h"
