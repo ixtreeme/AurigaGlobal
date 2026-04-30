dc620b7e64acb71a489c5e177b77eefc5973008f
ixtreeme <114311627+ixtreeme@users.noreply.github.com>
2026-04-30 13:23:31 +0200

    Phase 16-2: Migrate logging format in item_manager_idrange.cpp

--- SRC/Server/GameServer/item_manager_idrange.cpp
+++ SRC/Server/GameServer/item_manager_idrange.cpp
@@ -1,5 +1,6 @@
 
 #include "stdafx.h"
+#include <Core/Logging.hpp>
 #include "desc_client.h"
 #include "item_manager.h"
 
@@ -9,7 +10,7 @@ int touch(const char *path)
 
 	if ( !(fp = fopen(path, "a")) )
 	{
-		sys_err("touch failed");
+		LOG_ERROR("touch failed");
 		return (-1);
 	}
 
@@ -25,15 +26,14 @@ uint32_t ITEM_MANAGER::GetNewID()
 	{
 		if ( m_ItemIDSpareRange.dwMin == 0 || m_ItemIDSpareRange.dwMax == 0 || m_ItemIDSpareRange.dwUsableItemIDMin == 0 )
 		{
-			for ( int i=0; i < 10; i++ ) sys_err("ItemIDRange: FATAL ERROR!!! no more item id");
+			for ( int i=0; i < 10; i++ ) LOG_ERROR("ItemIDRange: FATAL ERROR!!! no more item id");
 			touch(".killscript");
 			thecore_shutdown();
 			return 0;
 		}
 		else
 		{
-			sys_log(0, "ItemIDRange: First Range is full. Change to SpareRange %u ~ %u %u",
-					m_ItemIDSpareRange.dwMin, m_ItemIDSpareRange.dwMax, m_ItemIDSpareRange.dwUsableItemIDMin);
+			LOG_INFO("ItemIDRange: First Range is full. Change to SpareRange {} ~ {} {}", m_ItemIDSpareRange.dwMin, m_ItemIDSpareRange.dwMax, m_ItemIDSpareRange.dwUsableItemIDMin);
 
 			db_clientdesc->DBPacket(HEADER_GD_REQ_SPARE_ITEM_ID_RANGE, 0, &m_ItemIDRange.dwMax, sizeof(uint32_t));
 
@@ -54,7 +54,7 @@ bool ITEM_MANAGER::SetMaxItemID(TItemIDRangeTable range)
 
 	if ( m_ItemIDRange.dwMin == 0 || m_ItemIDRange.dwMax == 0 || m_ItemIDRange.dwUsableItemIDMin == 0 )
 	{
-		for ( int i=0; i < 10; i++ ) sys_err("ItemIDRange: FATAL ERROR!!! ITEM ID RANGE is not set.");
+		for ( int i=0; i < 10; i++ ) LOG_ERROR("ItemIDRange: FATAL ERROR!!! ITEM ID RANGE is not set.");
 		touch(".killscript");
 		thecore_shutdown();
 		return false;
@@ -62,7 +62,7 @@ bool ITEM_MANAGER::SetMaxItemID(TItemIDRangeTable range)
 
 	m_dwCurrentID = range.dwUsableItemIDMin;
 
-	sys_log(0, "ItemIDRange: %u ~ %u %u", m_ItemIDRange.dwMin, m_ItemIDRange.dwMax, m_dwCurrentID);
+	LOG_INFO("ItemIDRange: {} ~ {} {}", m_ItemIDRange.dwMin, m_ItemIDRange.dwMax, m_dwCurrentID);
 
 	return true;
 }
@@ -71,14 +71,13 @@ bool ITEM_MANAGER::SetMaxSpareItemID(TItemIDRangeTable range)
 {
 	if ( range.dwMin == 0 || range.dwMax == 0 || range.dwUsableItemIDMin == 0 )
 	{
-		for ( int i=0; i < 10; i++ ) sys_err("ItemIDRange: FATAL ERROR!!! Spare ITEM ID RANGE is not set");
+		for ( int i=0; i < 10; i++ ) LOG_ERROR("ItemIDRange: FATAL ERROR!!! Spare ITEM ID RANGE is not set");
 		return false;
 	}
 
 	m_ItemIDSpareRange = range;
 
-	sys_log(0, "ItemIDRange: New Spare ItemID Range Recv %u ~ %u %u",
-			m_ItemIDSpareRange.dwMin, m_ItemIDSpareRange.dwMax, m_ItemIDSpareRange.dwUsableItemIDMin);
+	LOG_INFO("ItemIDRange: New Spare ItemID Range Recv {} ~ {} {}", m_ItemIDSpareRange.dwMin, m_ItemIDSpareRange.dwMax, m_ItemIDSpareRange.dwUsableItemIDMin);
 
 	return true;
 }
